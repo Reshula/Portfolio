398b4292322795736e4577670ee8eea4d317edba
Reshula <reyhana328@gmail.com>
2025-01-05 20:47:57 -0500

    were added new app, changed img and style

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Не получилось чтоb надпись была на анимации/
 gsap.to(".parOne", {
   text:"Hello , my name is Reyhana. <br>I am a  web developer.", 
-  duration:2.5,
+  duration:4.5,
   repeat:-1,
   repeatDelay:.7,
   ease:"power1.in"
 })
-gsap.from(".one",{
+gsap.from(".about-img",{
   delay:2,
   duration:4,
   ease:"pover1.in",
@@ -23,140 +23,270 @@ gsap.from(".parTwo",{
 
 })
 AOS.init({
-  duration: 2000,
-})
-////// Animation start/////////
+  duration: 2000})
 
-particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 380,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 140,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true
+// REYHANA
+
+gsap.from(".img",{y:-800,ease:"bounce",duration:4.5,opacity:0,delay:1,stagger:0.6,repeat:3})
+
+// CURSOR
+
+const cursorContainer = document.getElementById("cursor-container");
+
+
+const circleCount = 20;
+const circles = [];
+
+
+for (let i = 0; i < circleCount; i++) {
+  const circle = document.createElement("div");
+  circle.classList.add("cursor");
+  cursorContainer.appendChild(circle);
+  circles.push(circle);
+}
+
+
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY; 
+});
+
+function animateCircles() {
+  let x = mouseX;
+  let y = mouseY;
+
+  circles.forEach((circle, index) => {
+    setTimeout(() => {
+      circle.style.top = `${y}px`; //
+      circle.style.left = `${x}px`;
+      circle.style.transform = `scale(${1 - index * 0.05})`; 
+      circle.style.opacity = `${1 - index * 0.1}`; 
+    }, index * 20); 
   });
-  
-  
-  /* ---- stats.js config ---- */
-  
-  var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector('.js-count-particles');
-  update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  }
-  requestAnimationFrame(update);
 
-////// Animation finished//////
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+// Эффект "expand" при клике
+document.addEventListener("click", () => {
+  circles[0].classList.add("expand");
+  setTimeout(() => {
+    circles[0].classList.remove("expand");
+  }, 500);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
